10f66631d67a674e9f7faa1aef2791581c2950a5
Vincent Ging Ho Yim <cenviity@gmail.com>
2024-10-11 11:55:48 +1100

    cli: use singular form when only 1 commit is abandoned after Git import

--- cli/tests/test_git_fetch.rs
+++ cli/tests/test_git_fetch.rs
@@ -1154,7 +1154,7 @@ fn test_git_fetch_some_of_many_bookmarks(subprocess: bool) {
     ------- stderr -------
     bookmark: a1@origin [updated] tracked
     bookmark: b@origin  [updated] tracked
-    Abandoned 1 commits that are no longer reachable.
+     Abandoned 1 commit that is no longer reachable.
     [EOF]
     ");
     }
@@ -1201,7 +1201,7 @@ fn test_git_fetch_some_of_many_bookmarks(subprocess: bool) {
     insta::assert_snapshot!(output, @r"
     ------- stderr -------
     bookmark: a2@origin [updated] tracked
-    Abandoned 1 commits that are no longer reachable.
+     Abandoned 1 commit that is no longer reachable.
     [EOF]
     ");
     }
@@ -1866,7 +1866,7 @@ fn test_git_fetch_removed_bookmark(subprocess: bool) {
     insta::assert_snapshot!(output, @r"
     ------- stderr -------
     bookmark: a2@origin [deleted] untracked
-    Abandoned 1 commits that are no longer reachable.
+    Abandoned 1 commit that is no longer reachable.
     [EOF]
     ");
     }
@@ -1972,7 +1972,7 @@ fn test_git_fetch_removed_parent_bookmark(subprocess: bool) {
     ------- stderr -------
     bookmark: a1@origin     [deleted] untracked
     bookmark: trunk1@origin [deleted] untracked
-    Abandoned 1 commits that are no longer reachable.
+    Abandoned 1 commit that is no longer reachable.
     Warning: No branch matching `master` found on any specified/configured remote
     [EOF]
     ");
